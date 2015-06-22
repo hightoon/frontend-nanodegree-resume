1f893c9d44bdd340ad0733f59a2fcec309095597
hightoon <haitong.chen@gmail.com>
2015-06-22 10:23:09 +0800

    clean objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,13 +7,62 @@ formattedRole = HTMLheaderRole.replace("%data%", "Sofware Engineer");
 
 $("#header").append(formattedName).append(formattedRole);
 
+var bio = {
+    "name": "Haitong Chen",
+    "position": "Software Engineer",
+    "age": 34,
+    "location": "Hangzhou"
+};
+
+var work = {
+    "experience": [
+        {
+            "company": "Nokia Newworks",
+            "years": "2011 - present",
+            "position": "Software Specialist",
+        },
+        {
+            "company": "Motorola",
+            "years": "2008-2011",
+            "position": "Senior Software Engineer"
+        },
+        {
+            "company": "ZTE",
+            "years": "2006-2008",
+            "position": "Embedded Software Engineer"
+        }
+    ]
+};
+
+var project = {
+    "motoProjects": [
+        {
+            "name": "RUA/RUI",
+            "description": "RUA/RUI feature for Motorola Dimetra PDR",
+            "skills": ["C", "Linux", "Socket programming", "TCP/UP"]
+        },
+        {
+            "name": "PDR performance testing tool",
+            "description": "Performance testing tool design and development",
+            "skills": ["Python", "Linux", "Socket programming"]
+        }
+    ],
+    "nokiaProjects": [
+        {
+            "name": "WBTS Software development & maintenance",
+            "description": "new feature development, bug fix, problem-solving",
+            "skills": ["embedded", "DSP", "C", "OSEck"]
+        }
+    ]
+};
+
 var education = {
     "schools": [
      {
         "school": "Nanjing University of Aero. & Astro",
         "city": "Nanjing",
         "major": "Mechanical engineering",
-        "graduation": "2003"	
+        "graduation": "2003"
     },
     {
         "school": "Nanjing University of Aero. & Astro",
@@ -25,7 +74,6 @@ var education = {
     {
         "title": "Javascript basics",
         "school": "Udacity",
-        "date": "2015"   
+        "date": "2015"
     }]
 };
-
