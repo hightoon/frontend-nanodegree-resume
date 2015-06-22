2978d991dc23f904ff8e15051f01d4a4ddfd9b4a
Haitong <haitong.chen@gmail.com>
2015-06-22 01:22:38 +0000

    add education object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,3 +6,26 @@ formattedName = HTMLheaderName.replace("%data%", "Haitong Chen");
 formattedRole = HTMLheaderRole.replace("%data%", "Sofware Engineer");
 
 $("#header").append(formattedName).append(formattedRole);
+
+var education = {
+    "schools": [
+     {
+        "school": "Nanjing University of Aero. & Astro",
+        "city": "Nanjing",
+        "major": "Mechanical engineering",
+        "graduation": "2003"	
+    },
+    {
+        "school": "Nanjing University of Aero. & Astro",
+        "city": "Nanjing",
+        "major": "Automation engineering",
+        "graduation": "2006"
+    }],
+    "onlineCourses": [
+    {
+        "title": "Javascript basics",
+        "school": "Udacity",
+        "date": "2015"   
+    }]
+};
+
